ae6c0ade59e5cc177d2a82b851ff126e0f747136
sadamdjon@gmail.com <sadamjondjabarov@gmail.com>
2023-02-15 18:22:54 +0300

    new-version

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,17 @@
 const hamburger = document.querySelector('.hamburger');
 const cloEm = document.querySelector('.menu__close');
 const menu = document.querySelector('.menu');
-
+const menuList = document.querySelector('.menu__list');
 hamburger.addEventListener('click', function () {
   menu.classList.add('active');
 });
 cloEm.addEventListener('click', function () {
   menu.classList.remove('active');
+});
+
+menuList.addEventListener('click', function () {
+  menu.classList.add('close');
+});
+hamburger.addEventListener('click', function () {
+  menu.classList.remove('close');
 });
