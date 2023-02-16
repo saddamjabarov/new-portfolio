43fa963727db0f0aed1bb8870a50bfe97f6b90b0
sadamdjon@gmail.com <sadamjondjabarov@gmail.com>
2023-02-16 19:24:31 +0300

    menu closes when you click on in the outer menu area

--- src/js/script.js
+++ src/js/script.js
@@ -4,18 +4,22 @@ const menu = document.querySelector('.menu');
 const menuList = document.querySelector('.menu__list');
 const menuItem = document.querySelectorAll('.menu__item');
 const close = document.querySelector('.close');
+const menuOverlay = document.querySelector('.menu__overlay');
 hamburger.addEventListener('click', function () {
   menu.classList.add('active');
 });
-cloEm.addEventListener('click', function () {
+cloEm.addEventListener('click', () => {
   menu.classList.remove('active');
 });
 hamburger.addEventListener('click', () => {
   menu.classList.remove('close');
 });
 menuItem.forEach((item) => {
-  item.addEventListener('click', function () {
+  item.addEventListener('click', () => {
     item.classList.toggle('close');
     menu.classList.toggle('close');
   });
+});
+menuOverlay.addEventListener('click', () => {
+  menu.classList.add('close');
 });
