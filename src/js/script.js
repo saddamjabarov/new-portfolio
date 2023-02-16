c258bc126ab0eeae01ede1332cc0198d5b98fc7a
sadamdjon@gmail.com <sadamjondjabarov@gmail.com>
2023-02-16 15:10:38 +0300

    script menu link 12 minutes

--- src/js/script.js
+++ src/js/script.js
@@ -2,16 +2,20 @@ const hamburger = document.querySelector('.hamburger');
 const cloEm = document.querySelector('.menu__close');
 const menu = document.querySelector('.menu');
 const menuList = document.querySelector('.menu__list');
+const menuItem = document.querySelectorAll('.menu__item');
+const close = document.querySelector('.close');
 hamburger.addEventListener('click', function () {
   menu.classList.add('active');
 });
 cloEm.addEventListener('click', function () {
   menu.classList.remove('active');
 });
-
-menuList.addEventListener('click', function () {
-  menu.classList.add('close');
-});
-hamburger.addEventListener('click', function () {
+hamburger.addEventListener('click', () => {
   menu.classList.remove('close');
+});
+menuItem.forEach((item) => {
+  item.addEventListener('click', function () {
+    item.classList.toggle('close');
+    menu.classList.toggle('close');
+  });
 });
